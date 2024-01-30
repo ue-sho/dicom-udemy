f6962ed7d0b41ec56f9d23ff2a2bae60220d0103
ue-sho <tssu45@gmail.com>
2024-01-30 16:22:40 +0900

    Anonymize DICOM

--- dicom_handler.py
+++ dicom_handler.py
@@ -72,6 +72,55 @@ def normalize_visualize_dicom_2(
     return uint8_image
 
 
+def anonymize_dicom(
+    dcm_file: str,
+    patient_name: str | None = None,
+    patient_birthdate: str | None = None,
+    patient_sex: str | None = None,
+    study_id: str | None = None,
+    save_path: str | None = None,
+) -> pydicom.Dataset:
+    """anonymize DICOM file
+
+    Args:
+        dcm_file (str): DICOM file path
+        patient_name (str | None, optional): patient name. Defaults to None.
+        patient_birthdate (str | None, optional): patient birth date. Defaults to None.
+        patient_sex (str | None, optional): patient sex. Defaults to None.
+        study_id (str | None, optional): study ID. Defaults to None.
+        save_path (str | None, optional): save path. Defaults to None.
+
+    Returns:
+        pydicom.Dataset: anonymized DICOM file
+    """
+    dicom_file = pydicom.dcmread(dcm_file)
+
+    if patient_name:
+        dicom_file.PatientName = patient_name
+    else:
+        dicom_file.PatientName = ""
+
+    if patient_birthdate:
+        dicom_file.PatientBirthDate = patient_birthdate
+    else:
+        dicom_file.PatientBirthDate = ""
+
+    if patient_sex:
+        dicom_file.PatientSex = patient_sex
+    else:
+        dicom_file.PatientSex = ""
+
+    if study_id:
+        dicom_file.StudyID = study_id
+    else:
+        dicom_file.StudyID = ""
+
+    if save_path:
+        dicom_file.save_as(save_path)
+
+    return dicom_file
+
+
 if __name__ == "__main__":
     normalize_visualize_dicom_1("data/CT/1-01.dcm", show=True)
     normalize_visualize_dicom_2(
